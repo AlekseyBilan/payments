dd909d78bbde92fbaa7f3690acc6ccd3c6aef2bd
AlekseyBilan <bilan.aleksey.sergeevich@gmail.com>
2016-02-03 18:05:54 +0200

    refactor

--- js/app.js
+++ js/app.js
@@ -6,10 +6,8 @@ var newPaymentModel = new NewPaymentModel;
 
 var newPaymentView = new NewPaymentView({ model: newPaymentModel });
 $('.form-new-pay').append(newPaymentView.render().el);
-console.log('newPaymentView.render().el ',newPaymentView.render().el);
 
 var paymentListCollection = new PaymentListCollection();
 
 var paymentCollectionView = new PaymentCollectionView({collection: paymentListCollection});
 $('.wrap-tcontent').append(paymentCollectionView.render().el);
-console.log('paymentCollectionView.render().el', paymentCollectionView.render().el);
