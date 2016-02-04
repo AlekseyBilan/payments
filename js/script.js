23ff70c5a69b923ee783ad73eb8f01d5deca8ee8
AlekseyBilan <bilan.aleksey.sergeevich@gmail.com>
2016-02-04 15:19:22 +0200

    добавлено закрашивание бекграунда, совпавшего текста при поиске

--- js/script.js
+++ js/script.js
@@ -14,15 +14,15 @@ $(function(){
   });*/
 
 	// поиск на JQuery TODO - make search by Backbone
-	 var search = $(".search");
+	 var search = $(".search"), searchText;
     search.keyup(function(){
+        searchText = search.val();
       $('.table-pay tr').each(function(){
-        if($(this).text().indexOf(search.val()) === -1) {
+        if($(this).text().indexOf(searchText) === -1) {
           $(this).hide();
         } else {
           $(this).show();
         }
       });
     });
-
 });
